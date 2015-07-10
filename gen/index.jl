d4413219773171771dc50e4d0ae416453f0c56dd
Zachary Yedidia <zyedidia@gmail.com>
2015-07-10 17:34:44 -0400

    Start of lua rewrite

--- gen/index.jl
+++ gen/index.jl
@@ -7,15 +7,15 @@ end
 
 
 function section_for_linenum(linenum)
-# Get the section number given a line number
-for i = 1:length(section_linenums)
-    if i == length(section_linenums)
-        return i
+    # Get the section number given a line number
+    for i = 1:length(section_linenums)
+        if i == length(section_linenums)
+            return i
+        end
+        if linenum < section_linenums[i + 1]
+            return i
+        end
     end
-    if linenum < section_linenums[i + 1]
-        return i
-    end
-end
 
 end
 
@@ -27,7 +27,7 @@ function create_index(inputfile)
         println("If you do not want to make an index use the -noindex flag.")
         return ""
     end
-        
+
     supported_languages = split(lowercase(readall(`ctags --list-languages`)), "\n")
     if !(lowercase(codetype) in supported_languages)
         println("$codetype is not supported by your version of ctags.")
@@ -35,53 +35,53 @@ function create_index(inputfile)
         return ""
     end
 
-# Run Ctags on the lit file
-tags_str = readall(`ctags -x --language-force=$(lowercase(codetype)) $inputfile`)
+    # Run Ctags on the lit file
+    tags_str = readall(`ctags -x --language-force=$(lowercase(codetype)) $inputfile`)
 
-tags_arr = split(tags_str, "\n")
-tags = Tag[]
+    tags_arr = split(tags_str, "\n")
+    tags = Tag[]
 
-for tag in tags_arr
-    if tag != ""
-        words = split(tag, r"\s+")
-        line_num = parse(Int, words[3])
+    for tag in tags_arr
+        if tag != ""
+            words = split(tag, r"\s+")
+            line_num = parse(Int, words[3])
 
-        if !(line_num in code_lines)
-            continue
+            if !(line_num in code_lines)
+                continue
+            end
+
+            name = words[1]
+            tag_type = words[2]
+
+            push!(tags, Tag(name, tag_type, line_num))
         end
-
-        name = words[1]
-        tag_type = words[2]
-        
-        push!(tags, Tag(name, tag_type, line_num))
     end
-end
 
-# Create the HTML for the index
-html = "<h3>Index</h3>\n"
-html *= "<h5>Identifiers Used</h5>\n"
-html *= "<ul class=\"two-col\">\n"
+    # Create the HTML for the index
+    html = "<h3>Index</h3>\n"
+    html *= "<h5>Identifiers Used</h5>\n"
+    html *= "<ul class=\"two-col\">\n"
 
-for tag in tags
-    section_num = section_for_linenum(tag.line_num)
-    html *= "<li><code>$(tag.name)</code>: <em>$(tag.tag_type)</em> <a href=\"#$section_num\">$section_num</a></li>\n"
-end
-html *= "</ul>"
-
-html *= "<h5>Code Blocks</h5>\n"
-html *= "<ul class=\"two-col\">\n"
-
-# Sort the block_locations dictionary so that the codeblocks come in order
-for (name, locations) in sort(collect(block_locations), by=x->x[2][1])
-    html *= "<li><code>$(name)</code>"
-    for i = 1:length(locations)
-        location = locations[i]
-        p = i == 1 ? " " : ", "
-        html *= "$p<a href=\"#$(location)\">$(location)</a>"
+    for tag in tags
+        section_num = section_for_linenum(tag.line_num)
+        html *= "<li><code>$(tag.name)</code>: <em>$(tag.tag_type)</em> <a href=\"#$section_num\">$section_num</a></li>\n"
     end
-    html *= "</li>\n"
-end
-html *= "</ul>"
+    html *= "</ul>"
+
+    html *= "<h5>Code Blocks</h5>\n"
+    html *= "<ul class=\"two-col\">\n"
+
+    # Sort the block_locations dictionary so that the codeblocks come in order
+    for (name, locations) in sort(collect(block_locations), by=x->x[2][1])
+        html *= "<li><code>$(name)</code>"
+        for i = 1:length(locations)
+            location = locations[i]
+            p = i == 1 ? " " : ", "
+            html *= "$p<a href=\"#$(location)\">$(location)</a>"
+        end
+        html *= "</li>\n"
+    end
+    html *= "</ul>"
 
     return html
 end
