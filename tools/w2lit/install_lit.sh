5766069d195fd32615f394b8c223efd1f3d4c523
Zachary Yedidia <zyedidia@gmail.com>
2016-01-16 15:56:17 -0500

    Remove tools

--- tools/w2lit/install_lit.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/zyedidia/Literate
-cd Literate
-rm -rf src lua examples tools
-printf '.' | ./install_lua.sh
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-sed "1s%.*%#!$DIR/lua%" ./bin/lit > lit
-chmod +x lit
-rm -rf bin
